2baff1e2c47e1213e8605a8b7232c89da14fcf89
Antony Maziero Warken <againplayi7@gmail.com>
2022-06-23 15:17:16 -0300

    cap 2 finalizado

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -17,5 +17,13 @@ module.exports = {
             throw new Error('Fornecedor não encontrado')
         }
         return encontrado
+    },
+    atualizar(id, dadosParaAtualizar) {
+        return Modelo.update(
+            dadosParaAtualizar,
+            {
+                where: {id: id}
+            }
+        )
     }
 }
--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -24,7 +24,23 @@ roteador.get('/:idFornecedor', async (req, res) => {
             JSON.stringify(fornecedor)
         )
     } catch(erro) {
-        resposta.send(
+        res.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        )
+    }
+})
+roteador.put('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const dadosRecebidos = req.body
+        const dados = Object.assign({}, dadosRecebidos, {id: id})
+        const fornecedor = new Fornecedor(dados)
+        await fornecedor.atualizar()
+        res.end()
+    } catch(erro) {
+        res.send(
             JSON.stringify({
                 mensagem: erro.message
             })
