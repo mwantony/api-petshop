9bf470154d6f40494eaa839b567cc751371fd7ff
Antony Maziero Warken <againplayi7@gmail.com>
2022-06-22 21:14:26 -0300

    cap 1 vid 1 finalizado

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+const config = require('config')
 app.use(bodyParser.json())
 
-app.listen(3000, () => console.log('A API está funcionando!'))
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
