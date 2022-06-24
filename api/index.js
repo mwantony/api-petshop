c71d8487cb7887eb83cf087f5b33899a1cab7e06
Antony Maziero Warken <againplayi7@gmail.com>
2022-06-24 13:53:46 -0300

    cap 4 finalizado

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const config = require("config");
 const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 app.use(bodyParser.json());
 
 const roteador = require("./rotas/fornecedores");
@@ -17,6 +18,9 @@ app.use((erro, req, res, proximo) => {
   } 
   if(erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
     status = 400
+  }
+  if(erro instanceof ValorNaoSuportado) {
+    status = 406
   }
   res.status(status)
   res.send(
